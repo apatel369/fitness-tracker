d930b78b15aff9c02ca7acf519688b080233dd28
Arpan Patel <ap.patel1893@gmail.com>
2019-09-09 11:02:49 -0500

    add date validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,12 +7,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  maxDate;
 
   constructor() { }
 
   ngOnInit() {
-
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
+
   onSubmit(form: NgForm){
       console.log(form);
   }
