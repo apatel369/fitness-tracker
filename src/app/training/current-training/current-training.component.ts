f0f38ffb28a2f9b52a3b564a0da0b1d8dd503d50
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-10 00:12:08 -0500

    add exit and continue options

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { StopTrainingComponent } from './stop-traiining.component'
@@ -9,6 +9,7 @@ import { StopTrainingComponent } from './stop-traiining.component'
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
@@ -23,14 +24,28 @@ export class CurrentTrainingComponent implements OnInit {
     }, 1000)
   }
 
+  startoOrResumeTimer() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 5;
+      if(this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 1000);
+  }
+
   onStop() {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
       data: {
       progress: this.progress
     }});
+
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if(result) {
+        this.trainingExit.emit();
+      } else {
+        this.startoOrResumeTimer();
+      }
     })
   }
 
