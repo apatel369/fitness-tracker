a1e0febeae12dc5f3c7c2d5312ff6786f3af2e96
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-09 23:06:15 -0500

    add timer

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: number;
 
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 5;
+      if(this.progress >= 100) {
+        clearInterval(this.timer);
+      }
     }, 1000)
+  }
+
+  onStop() {
+    clearInterval(this.timer);
   }
 
 }
