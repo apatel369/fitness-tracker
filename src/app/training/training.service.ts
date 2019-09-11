c90e1ba1d56c02ba3a61b7e9a6e9fc93f83ba926
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-11 18:26:52 -0500

    handling complete and cancel events

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -13,6 +13,7 @@ export class TrainingService {
   ]
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] =  [];
 
   getAvailableExercises() {
     return this.availableExercises.slice();
@@ -21,6 +22,23 @@ export class TrainingService {
   startExercise(selectedId: string) {
     this.runningExercise = this.availableExercises.find(exercise => exercise.id === selectedId);
     this.exerciseChanged.next({...this.runningExercise });
+  }
+
+  completeExercise() {
+    this.exercises.push({...this.runningExercise, date: new Date(), state: 'completed'});
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories:  this.runningExercise.duration * (progress / 100),
+      date: new Date(),
+      state: 'cancelled'});
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
   }
 
   getRunningExercise() {
