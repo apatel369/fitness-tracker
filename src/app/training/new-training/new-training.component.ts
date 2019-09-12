f8eba283a49c71efdd9f07c62fb9d1b36bee091e
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-12 00:03:23 -0500

    add snapshot changes

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -5,6 +5,8 @@ import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-new-training',
@@ -12,15 +14,27 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
     this.exercises = this.db.
     collection('availableExercises')
-    .valueChanges();
-  }
+    .snapshotChanges()
+    .pipe(
+      map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data()['name'],
+            duration: doc.payload.doc.data()['duration'],
+            calories: doc.payload.doc.data()['calories']
+          };
+        });
+      })
+    );
+  }rve
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
