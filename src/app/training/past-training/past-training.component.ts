20b6e8386646d0065102dfcefc63f913b606d46e
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-11 21:32:17 -0500

    add filtering

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -25,4 +25,8 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
     this.dataSource.sort = this.sort;
   }
 
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
