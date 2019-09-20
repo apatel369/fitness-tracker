9350b45ce83a58c9fd36de8f235e71c49ee10e88
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-19 22:48:42 -0500

    split into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { TrainingComponent } from './training/training.component';
-import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
-import { NewTrainingComponent } from './training/new-training/new-training.component';
-import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './navigation/header/header.component';
@@ -25,22 +19,17 @@ import { AuthService } from './auth/auth.service';
 import { TrainingService } from './training/training.service';
 import { environment } from 'src/environments/environment';
 import { UIService } from './shared/ui.service';
+import { AuthModule } from './auth/auth.module,';
+import { TrainingModule } from './training/training.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
-    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -49,13 +38,14 @@ import { UIService } from './shared/ui.service';
     AppRoutingModule,
     FlexLayoutModule,
     FormsModule,
+    TrainingModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    AuthModule
   ],
   providers: [AuthService, TrainingService, UIService],
   bootstrap: [AppComponent],
-  entryComponents: [StopTrainingComponent]
 })
 export class AppModule { }
---
+++ src/app/training/training.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { TrainingComponent } from './training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingComponent } from './past-training/past-training.component';
+import { StopTrainingComponent } from './current-training/stop-training.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent,
+    StopTrainingComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    AngularFirestoreModule
+   ],
+  entryComponents: [StopTrainingComponent]
+})
+export class TrainingModule {}
