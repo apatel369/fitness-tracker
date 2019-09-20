3ec93e90bea2d339829acd48167f26cb22b1ca18
apatel369 <33442948+apatel369@users.noreply.github.com>
2019-09-20 00:11:00 -0500

    add firebase key in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDqkGrG85ThZ7cKV5YlwSwPgdAMIA8wXPw",
+    authDomain: "ng-fitness-tracker-58092.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-58092.firebaseio.com",
+    projectId: "ng-fitness-tracker-58092",
+    storageBucket: "ng-fitness-tracker-58092.appspot.com",
+    messagingSenderId: "1089574028778",
+    appId: "1:1089574028778:web:cbbf3da576e911e7025e93"
+  }
+}
